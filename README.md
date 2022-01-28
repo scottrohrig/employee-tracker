27dc2af489b1b7ecda4d4f3ab103a3e9fe0fd67b
scottrohrig <scott.rohrig@gmail.com>
2022-01-27 16:33:53 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Employee Tracker CMS
+
+## Description
+
+- A content management system CLI to manage a company's employee database of departments, roles and employees
+
+## Uses
+
+- Node.js, Inquirer, & MySQL
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require( 'express' );
+const db = require( './db/connection' );
+const routes = require( './routes' );
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use( express.urlencoded( { extended: true } ) );
+app.use( express.json() );
+
+app.use( '/', routes );
+
+db.connect( err => {
+    if ( err ) throw err;
+    console.log( '\nDatabase Connected' );
+    app.listen( PORT, () => {
+        console.log( `Server live! Running at http://localhost:${ PORT }` );
+    } );
+} );
